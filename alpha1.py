2c172747d7e40125d75095eb55ad3bc87d8ab5b2
Rohan <rohanmaheshrao10@gmail.com>
2022-10-19 12:45:43 +0530

    removed pyfirmata

--- alpha1.py
+++ alpha1.py
@@ -2,60 +2,6 @@ import cv2
 import numpy as np
 
 ################################# LIBS ABOVE #############
-from pyfirmata import Arduino
-import pyfirmata
-import urllib
-
-# ir1 = 8
-# istripped = 0
-
-# irvalue = 0
-
-# comms = Arduino("COM4")
-# comms.digital[ir1].mode = pyfirmata.INPUT
-# print("Started Firmata Comms... Version:",comms.get_firmata_version())
-
-# def ir_start():
-
-# 	global irvalue
-# 	it = pyfirmata.util.Iterator(comms)
-# 	it.start()
-# 	irvalue = comms.digital[ir1].read()
-
-# def trip():
-
-#     global istripped
-#     global ir1
-
-#     if (comms.digital[ir1].read(1)):
-#         istripped = 1
-
-#     if (comms.digital[ir1].read(0)):
-#         istripped = 0
-    
-# def cloud_push(data):
-
-# 	URL = 'https://api.thingspeak.com/update?api_key='
-# 	KEY = 'S7Z35PBVKENP386F'
-# 	HEADER = '&field1={}'.format(data)
-# 	new_URL = URL + KEY + HEADER
-
-# 	pushed_url = urllib.request.urlopen(new_URL)
-# 	print(pushed_url)
-	
-
-#######################################################################################
-# Write api key =  S7Z35PBVKENP386F
-# read api key =  8HMKSPCLNMVG7EAH
-#######################################################################################
-
-################################# INITS BELOW ############
-ir1 = 8
-comms = Arduino('COM5')
-comms.digital[ir1].mode = pyfirmata.INPUT
-
-istripped = 0
-
 frame = cv2.VideoCapture(0)
 
 def calc_dist(cx1,cy1,cx2,cy2):
@@ -64,9 +10,6 @@ def calc_dist(cx1,cy1,cx2,cy2):
         return ((cy2 - cy1)**2 + (cx2 - cx1)**2)**0.5
 
 while(True):    
-
-    # ir_start()
-    # trip()
 
     cx_red = 0
     cy_red = 0
@@ -128,9 +71,6 @@ while(True):
  
     euclidian_dist = calc_dist(cx_face,cy_face,cx_red,cy_red)
     print("Inter object distance:",euclidian_dist)
-
-    # if (istripped):
-    #     cloud_push(euclidian_dist)
        
     cv2.imshow("main window",feed)
     if cv2.waitKey(1) == ord('q'):
